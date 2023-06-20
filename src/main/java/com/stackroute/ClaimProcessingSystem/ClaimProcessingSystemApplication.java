1a01ff0656f7fa7754f1ab271dd638d89042ada1
osachin964 <osachin964@gmail.com>
2023-06-20 12:04:02 +0530

    init

---
+++ src/main/java/com/stackroute/ClaimProcessingSystem/ClaimProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.ClaimProcessingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimProcessingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackroute/ClaimProcessingSystem/ClaimProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.ClaimProcessingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimProcessingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
